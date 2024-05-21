577d04ccad8acb358442e5dfc151bcb16f9f252c
Tristan Jensen <tristanjensencodes@gmail.com>
2024-05-21 10:06:34 -0700

    added express starter

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(index.html);
+    console.log("Server has been started...");
+});
+
+app.listen(process.env.PORT);
